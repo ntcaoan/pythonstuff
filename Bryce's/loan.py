dea581daf4a6dceaed0195721b6411ab4ef7d4bf
Anna Valentino <pusheen1308@gmail.com>
2024-05-22 21:31:30 -0600

    yay the heatmap

--- Bryce's/loan.py
+++ Bryce's/loan.py
@@ -10,30 +10,43 @@ print(loan_data.head().to_string())
 
 # the occupation field is not needed for this
 loan_data.drop(columns=['occupation'], inplace=True)
-
+loan_copy = loan_data.copy()
 
 # Categorized Strings should be converted to integer Values for the analysis
+loan_copy['gender'] = loan_copy['gender'].map({0: "Male", 1: "Female"})
+male_data = loan_copy[loan_copy['gender'] == "Male"]
+female_data = loan_copy[loan_copy['gender'] == "Female"]
 
 
 # education level be broken to 3 types: High School, Bachelor's, Graduate Level
-
-loan_copy = loan_data.copy()
-loan_copy['education_level'] = loan_copy['education_level'].map({0: "Bachelor's", 1: "Graduate Level", 2: "High School"})
-
-bachelor_data = loan_copy[loan_copy['education_level'] == "Bachelor's"]
-high_school = loan_copy[loan_copy['education_level'] == "High School"]
-bachelor_data = loan_copy[loan_copy['education_level'] == "Master" and loan_copy['education_level'] == "Doctoral"]
-
-
-print(bachelor_data)
+def categorize_education(education):
+    if "High School" in education:
+        return 2
+    elif "Bachelor's" in education:
+        return 0
+    else:
+        return 1
 
 
+loan_copy['education_level'] = loan_copy['education_level'].apply(categorize_education)
 
+# Print the DataFrame to check the categorization
+print(loan_copy)
 
+# If you need to create separate DataFrames for each category:
+bachelor_data = loan_copy[loan_copy['education_level'] == 0]
+graduate_data = loan_copy[loan_copy['education_level'] == 1]
+high_school_data = loan_copy[loan_copy['education_level'] == 2]
+
+# Print the separate DataFrames to check
+print(f"Bachelor's Data: \n{bachelor_data}")
+
+print(f"Graduate Level Data: \n{graduate_data}")
+
+print(f"High School Data: \n{high_school_data}")
 
 # (3) Graphing
 # a. Heatmap for the given Dataframe
-# sns.heatmap(loan_data.corr(), annot=True)
-# plt.show()
-
-
+numeric_columns = loan_copy.select_dtypes(include=[int, int, int, int, float, int, int]).columns
+sns.heatmap(loan_copy[numeric_columns].corr(), annot=True)
+plt.show()
